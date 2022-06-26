91b46345e6314b5c168e4ef7ebde515a56c8c17b
Corvette31 <corvette.ss@yandex.ru>
2022-06-27 02:37:48 +0300

    Добавьте файлы проекта.

---
+++ ArrayTask1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ArrayTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int arrayColumns = 5;
+            int arryaRows = 5;
+            int itemMinValue = 1;
+            int itemMaxValue = 9;
+            int[,] array = new int[arryaRows, arrayColumns];
+            int row = 1;
+            int column = 0;
+            int sum = 0;
+            int productNumbers = 1;
+
+            Console.WriteLine("Двумерный массив: ");
+
+            for (int i = 0; i < arryaRows; i++)
+            {
+                for (int j = 0; j < arrayColumns; j++)
+                {
+                    array[i, j] = random.Next(itemMinValue, itemMaxValue);
+                    Console.Write($"{array[i, j]} ");
+
+                    if (i == row)
+                    {
+                        sum += array[i, j];
+                    }
+
+                    if (j == column)
+                    {
+                        productNumbers *= array[i, j];
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Сумма строки {++row} равна : {sum} , произвидение {++column} столбца равна: {productNumbers}");
+        }
+    }
+}
